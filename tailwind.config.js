a07e4f601ee10d149697734e462f7c17854ac3fb
Jorge Castillo <jorge@wilqo.com>
2024-03-15 13:13:57 -0700

    update image and ui fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx}',
